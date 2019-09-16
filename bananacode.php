86eec27181566aa0e02280cae8ba542953999c93
ando <ando@andonoMacBook-puro.local>
2019-09-16 17:10:53 +0900

    adding license etc.

--- bananacode.php
+++ bananacode.php
@@ -1,3 +1,11 @@
 <?php
 
+/**
+ * This is a sample code.
+ * @package bananacode
+ * @author bossatama
+ * @link https://github.com/bananacode-dev/bananacode/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
 echo 'Bananacode !';
